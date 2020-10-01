43745b31d6395315e9aac307c4aa67d8c96465fc
Andres Pacheco <andrespach4@gmail.com>
2020-10-01 13:47:35 -0500

    Remove conflicts indicators

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :admins
-=======
   resources :warehouses
->>>>>>> 8666aae... Add warehouses scaffold
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
